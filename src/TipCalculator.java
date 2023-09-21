e443172280494a29276f7596c5b819834e96376d
BT_2N2_03 <sample@bths.,edu>
2023-09-21 13:33:45 -0400

    total bill before tip (attempt 1)

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -8,13 +8,23 @@ public class TipCalculator {
         System.out.print("What percentage would you like to tip? (0-100):");
         int percentTip = scan.nextInt();
 
-        System.out.print("Enter a cost in dollars and cents (e.g. 12.50). When finished, enter -1");
+        System.out.print("Enter a cost in dollars and cents, e.g 12.50 (When finished, enter -1):");
+        double totalCost = 0;
         double number = scan.nextDouble();
         while (number != -1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (When finished, enter -1): ");
             number = scan.nextDouble();
+            totalCost += number;
+            while (number = -1) {
+            totalCost--;
+            }
+
+
+
         }
 
+        System.out.println("------------------------------------");
+        System.out.println("Total bill before tip: $" + totalCost);
 
     }
 }
