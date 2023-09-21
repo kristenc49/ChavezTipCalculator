33e0dfbfc54da62016c0c187da6a2b71a2ffd445
BT_2N2_03 <sample@bths.,edu>
2023-09-21 13:23:18 -0400

    "while" condition added (attempt 1)

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -5,7 +5,16 @@ public class TipCalculator {
         System.out.println("Welcome to the Tip Calculator!");
         System.out.print("How many people are in your party?");
         String ppl = scan.nextLine();
-        System.out.print("What percentage would you like to tip?");
+        System.out.print("What percentage would you like to tip? (0-100):");
+        int percentTip = scan.nextInt();
+
+        System.out.print("Enter a cost in dollars and cents (e.g. 12.50). When finished, enter -1");
+        double number = scan.nextDouble();
+        while (number != -1) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (When finished, enter -1): ");
+            number = scan.nextDouble();
+        }
+
 
     }
 }
