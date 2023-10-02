fa25161006e6edf511c89169cc64bcc20a3acc52
kristenchavez <kristenc49@nycstudents.net>
2023-10-02 02:17:25 -0400

    starting the ec

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,4 +1,7 @@
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 public class TipCalculator {
     public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
@@ -9,6 +12,7 @@ public class TipCalculator {
         int percentTip = scan.nextInt();
 
        // System.out.print("Enter a cost in dollars and cents, e.g 12.50 (When finished, enter -1):");
+        List items = new ArrayList();
         double totalCost = 0;
         double number = 2;
         while (number != -1) {
@@ -24,21 +28,31 @@ public class TipCalculator {
         System.out.println("Total percentage: " + percentTip + "%");
 
         // find out how to round everything below
-
-        double totalTip = ((double)percentTip/100) * totalCost;
+        double totalTip = (double) Math.round((((double)percentTip/100) * totalCost) * 100) / 100;
         System.out.println("Total tip: $" + totalTip);
 
         double totalBillWithTip = (totalCost + totalTip);
         System.out.println("Total bill with tip: $" + totalBillWithTip);
 
-        double perPersonCostBeforeTip = (totalCost / ppl);
+        double perPersonCostBeforeTip = (double) Math.round((totalCost / ppl) * 100) / 100;
         System.out.println("Per person cost before tip: $" + perPersonCostBeforeTip);
 
-        double tipPerPerson = (totalTip / ppl);
+        double tipPerPerson = (double) Math.round((totalTip / ppl) * 100) / 100;
         System.out.println("Tip per person: $" + tipPerPerson);
 
-        double totalCostPerPerson = (totalBillWithTip / ppl);
+        double totalCostPerPerson = (double) Math.round((totalBillWithTip / ppl) * 100) / 100;
         System.out.println("Total cost per person: $" + totalCostPerPerson);
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.print("Would you like to leave a note? (yes/no): ");
+        String noteYN = scan2.nextLine();
+        if (Objects.equals(noteYN, "yes")) {
+            System.out.print("Enter your note:");
+            String note = scan2.nextLine();
+            System.out.println("Note from customer: " + note);
+        } else {
+
+        }
 
         System.out.println("------------------------------------");
     }
